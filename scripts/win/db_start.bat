83aa0dbaea0f5149c0e09b325f2a7eb9e83ff31f
Will Garrett <willgarrett610@gmail.com>
2023-09-21 19:30:48 -0600

    Remove containers after killing

--- scripts/win/db_start.bat
+++ scripts/win/db_start.bat
@@ -1,3 +1,3 @@
 @echo off
 
-docker run --net bg_data_net --name bg_db -p 3306:3306 --mount type=volume,source=bg_data_vol,destination=/var/lib/mysql -d bg_db
+docker run --rm --net bg_data_net --name bg_db -p 3306:3306 --mount type=volume,source=bg_data_vol,destination=/var/lib/mysql -d bg_db
