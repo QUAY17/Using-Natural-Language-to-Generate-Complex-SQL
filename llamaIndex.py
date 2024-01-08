34f4e1c7d57a953eeae2dbccfee2e864f1f56eae
Jennifer Quay Minnich <jminniecc@gmail.com>
2024-01-08 11:15:00 -0700

    more llamaIndex but still getting API rate limit

--- llamaIndex.py
+++ llamaIndex.py
@@ -29,65 +29,48 @@ with engine.connect() as connection:
     for row in result:
         print(row)
 
-db_schema = '''create table report
-(
-    id                      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    excel_file_name         varchar(128) NOT NULL,
-    excel_tab_name          varchar(128) NOT NULL,
-    revenue_group           varchar(128),
-    business_activity_month DATE,
-    reporting_month         DATE,
-    distribution_month      DATE,
-    report_run              DATETIME,
-    start                   DATE,
-    end                     DATE
-);
-
-create table industry_entity
-(
-    id        INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name      varchar(128) NOT NULL,
-    entity_id INT NOT NULL,
-    foreign key (entity_id) references entity (id),
-    UNIQUE(name, entity_id)
-);
-
-create table entity
-(
-    id   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(128) NOT NULL,
-    code varchar(32) NOT NULL,
-    UNIQUE(name)
-);
-
-create table industry_entity_tax
-(
-    id                       BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    report_id                INT NOT NULL,
-    industry_entity_id       INT NOT NULL,
-    name                     varchar(128) NOT NULL,
-    returns                  DOUBLE,
-    total_receipts           DOUBLE,
-    taxable_receipts         DOUBLE,
-    matched_taxable_receipts DOUBLE,
-    total_tax_due            DOUBLE,
-    total_tax_paid           DOUBLE,
-    recipient_paid           DOUBLE,
-    recipient_due            DOUBLE,
-    adjustment               DOUBLE,
-    foreign key (report_id) references report (id),
-    foreign key (industry_entity_id) references industry_entity (id)
-);'''
-
 sql_database = SQLDatabase(engine, sample_rows_in_table_info=2)
 
 #print(sql_database.table_info)
 print(sql_database._usable_tables)
 print(list(sql_database._all_tables))
 print(sql_database.get_single_table_info("entity"))
-
-
+  
 OPEN_API_KEY = os.environ["OPENAI_API_KEY"]
+
+import tiktoken
+from llama_index.callbacks import CallbackManager, TokenCountingHandler
+token_counter = TokenCountingHandler(
+    tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode
+)
+
+callback_manager = CallbackManager([token_counter])
+     
+from llama_index import ServiceContext, LLMPredictor, OpenAIEmbedding, PromptHelper
+llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo")
+service_context = ServiceContext.from_defaults(
+  llm=llm,callback_manager=callback_manager
+)
+
+from llama_index.indices.struct_store.sql_query import NLSQLTableQueryEngine
+
+query_engine = NLSQLTableQueryEngine(
+    sql_database=sql_database,
+    service_context=service_context
+)
+     
+query_str = "how many entity are there?"
+response = query_engine.query(query_str)
+print(response.response)
+
+"""
+print()
+print(response.metadata['sql_query'])
+print()
+print(response.metadata['result'])
+print()
+print(token_counter.total_llm_token_count)"""
+
 """
 try:
     with mysql.connect(
